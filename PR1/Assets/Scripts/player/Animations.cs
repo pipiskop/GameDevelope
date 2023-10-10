29b2f2aeaad7173d8516077a8ec84eb53799ba2b
pipiskop <112812352+pipiskop@users.noreply.github.com>
2023-10-10 16:12:29 +0300

    10.10.2023

---
+++ PR1/Assets/Scripts/player/Animations.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            anim.SetBool("dance", true);
+        }
+        else
+        {
+            anim.SetBool("dance", false);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            anim.SetBool("Crouch", true);
+        }
+        else
+        {
+            anim.SetBool("Crouch", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("CrouchWalk", true);
+        }
+        else
+        {
+            anim.SetBool("CrouchWalk", false);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("BackWalk", true);
+        }
+        else
+        {
+            anim.SetBool("BackWalk", false);
+        }
+
+    }
+
+}
